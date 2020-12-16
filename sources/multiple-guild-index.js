ea8c8e12c7a3632b35b1f9d39b3ea7511efd269f
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-12-16 10:03:49 +0530

    Use for multiple guilds

---
+++ sources/multiple-guild-index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const newUsers = [];
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
+client.login("SuperSecretBotTokenHere");
