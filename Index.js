c6e48c954118ab6226adde52d2aa68ccaf24af26
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-12-19 20:21:43 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,21 +1,22 @@
-const Discord = require ("discord.js");
-const client = new Discord.Client();
-client.on("ready", () => {
-  console.log(`${client.user.tag} is now online!`);});
-  client.on("message", async message => {
-    if(message.content.startsWith('!ping'))
- { return message.channel.send (` Your Ping is ${client.ws.ping}ms`)
- }
-  });
-  client.on("guildMemberAdd", async member=> {
-    let channel = member.guild.channels.cache.find("OURCHANNELID")
-    let embed = new Discord.MessageEmbed()
-    .setTitle(`New User Has Joined!`)
-    .setDescription(`Hello, and welcome ${member.user.tag} We are happy to have you, now our server has ${member.guild.memberCount} users!`)
-    .setTimestamp()
-    .setColor('BLUE')
-    .setFooter('Welcome To Our Server')
-  message.channel.send(embed);
-  })
-  
-client.login("Nzg4NjYyMDE2MjM4ODEzMjM0.X9mwvQ.KREGoC06Eh8XY4czBwY8qLtMZmU")
+const Discord = require ("discord.js")
+const client = new Discord.Client()
+client.once("ready", () => {
+  console.log(`${client.user.tag} is now online!`)
+});
+client.on("message", async message =>{
+  if (message.content.startsWith("!ping"))
+  {
+    message.channel.send(`The client websocket latency is ${client.ws.ping}ms (values in milliseconds)`)
+  }
+});
+client.on("guildMemberAdd", async member => ,{
+  let channel = member.guild.channels.cache.find(c => c.name === 'welcome')
+  let WELCOME = new Discord.MessageEmbed()
+  .setTitle('New User Has Joined!')
+  .setDescription(`Welcome To Our Server ${member.tag} we are happy to have you! you are member number ${member.guild.memberCount}!`)
+  .setColor('BLUE')
+  .setTimestamp()
+  .setFooter('Thanks For Joining!')
+  channel.send(WELCOME)
+})
+client.login("Nzg5ODY3MzU5MDQ0MjM5Mzcw.X94TTQ.tS_SqfWVGaDYmiUTwSufmzcMW8s");
