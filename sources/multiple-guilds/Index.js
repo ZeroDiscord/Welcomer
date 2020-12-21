e771769bfd5815cdb1a55bb8a41560d3f9e3bb20
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-12-21 11:00:43 +0530

    Added code instructions (about to barf)

--- sources/multiple-guilds/Index.js
+++ sources/multiple-guilds/Index.js
@@ -1,33 +1,54 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require("discord.js"); // require the discord.js wrapper
+const client = new Discord.Client(); // Declare client to be a new Discord Client (bot)
 
-const newUsers = [];
-
+const newUsers = []; // a blank array for storing the newUser data
+/*
+Code Below provides info about the bot 
+once it's ready
+*/
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ\n[INFO] Bot serving on Ready to serve in ${client.guilds.cache.size} servers\n[INFO] Bot serving ${client.users.cache.size} users\n[Invite Link] https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
 });
-
+/* 
+Client when detects a message 
+then execute the code */
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  if (message.content.startsWith("!ping")) {
+  message.channel.send(`The client websocket latency is ${client.ws.ping}ms (values in milliseconds)`);
   }
 });
-
+/* 
+Client when detects 
+a new member join */
 client.on("guildMemberAdd", (member) => {
   const guild = member.guild;
   if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
   newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 10) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+/* 
+Below states if the members who joined are more than 5 in value then
+execute the following code to welcome all 5 together (can be set to any integral value (1,2,3,etc.)
+*/
+  if (newUsers[guild.id].size > 5) {
+    const userlist = newUsers[guild.id].map(userlist => userlist.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "welcome").send({embed: {
+  color: 3447003,
+       author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+  title: "This is an embed",
+  description: userlist
+}});
     newUsers[guild.id].clear();
   }
 });
-
+/*
+When a member leaves clear the cached info from the newUsers array
+just to mantain stability lol
+*/
 client.on("guildMemberRemove", (member) => {
   const guild = member.guild;
   if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
-client.login("SuperSecretBotTokenHere");
+client.login("BOTTOKENHERE"); // Login with the token make sure to add it when starting
