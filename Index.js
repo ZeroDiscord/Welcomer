62d8eae61b39bbd8c0cbf5082cba3bd5a645bcf0
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-12-21 10:29:41 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,22 +1,31 @@
-const Discord = require ("discord.js")
-const client = new Discord.Client()
+const Discord = require ("discord.js") // require the discord.js wrapper
+const client = new Discord.Client() // Declare client to be a new Discord Client (bot)
+/*
+Code Below provides info about the bot 
+once it's ready
+*/
 client.once("ready", () => {
   console.log(`${client.user.tag} is now online!`)
 });
+/* Client when detects a message 
+then execute the code */
 client.on("message", async message =>{
   if (message.content.startsWith("!ping"))
   {
     message.channel.send(`The client websocket latency is ${client.ws.ping}ms (values in milliseconds)`)
   }
 });
+/* Client when detects 
+a new member join */
 client.on("guildMemberAdd", async member => {
   let channel = member.guild.channels.cache.find(c => c.name === 'welcome')
   let WELCOME = new Discord.MessageEmbed()
   .setTitle('New User Has Joined!')
   .setDescription(`Welcome To Our Server ${member.user} we are happy to have you! you are member number ${member.guild.memberCount}!`)
   .setColor('BLUE')
+  .setThumbnail(client.user.avatarURL)
   .setTimestamp()
   .setFooter('Thanks For Joining!')
   channel.send(WELCOME)
 })
-client.login("Nzg5ODY3MzU5MDQ0MjM5Mzcw.X94TTQ.tS_SqfWVGaDYmiUTwSufmzcMW8s");
+client.login("BOTTOKENHERE"); // Login with the token make sure to add it when starting
