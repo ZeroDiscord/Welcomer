4ecadee15099a9ef0bedfd742e99cfd805b52a4d
Lukass <85770350+Lukass-0069@users.noreply.github.com>
2021-09-08 18:02:40 -0400

    Rewritten to Discord.js v13!

--- Index.js
+++ Index.js
@@ -1,31 +1,31 @@
-const Discord = require ("discord.js") // require the discord.js wrapper
+const Discord = require("discord.js") // require the discord.js wrapper
 const client = new Discord.Client() // Declare client to be a new Discord Client (bot)
-/*
-Code Below provides info about the bot 
-once it's ready
-*/
+
+// Code Below provides info about the bot once it's ready
 client.once("ready", () => {
   console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ\n[INFO] Bot serving on Ready to serve in ${client.guilds.cache.size} servers\n[INFO] Bot serving ${client.users.cache.size} users\n[Invite Link] https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`)
 });
-/* Client when detects a message 
-then execute the code */
-client.on("message", async message =>{
-  if (message.content.startsWith("!ping"))
-  {
-    message.channel.send(`The client websocket latency is ${client.ws.ping}ms (values in milliseconds)`)
+
+// Client when detects a message then execute the code 
+client.on("message", async message => {
+  if (message.content.startsWith("!ping")) {
+    message.channel.send({
+      content: `My ping is \`${client.ws.ping}\``
+    });
   }
 });
-/* Client when detects 
-a new member join */
+
+// Client when detects a new member join 
 client.on("guildMemberAdd", async member => {
   let channel = member.guild.channels.cache.find(c => c.name === 'welcome')
   let WELCOME = new Discord.MessageEmbed()
-  .setTitle('New User Has Joined!')
-  .setDescription(`Welcome To Our Server ${member.user} we are happy to have you! you are member number ${member.guild.memberCount}!`)
-  .setColor('BLUE')
-  .setThumbnail(client.user.avatarURL)
-  .setTimestamp()
-  .setFooter('Thanks For Joining!')
+    .setTitle('New User Has Joined!')
+    .setDescription(`Welcome To Our Server ${member.user} we are happy to have you! you are member number ${member.guild.memberCount}!`)
+    .setColor('BLUE')
+    .setThumbnail(client.user.avatarURL)
+    .setTimestamp()
+    .setFooter('Thanks For Joining!')
   channel.send(WELCOME)
-})
-client.login("BOTTOKENHERE"); // Login with the token make sure to add it when starting
+});
+
+client.login("bot-token-here"); // Login with the token make sure to add it when starting
--- sources/multiple-guilds/Index.js
+++ sources/multiple-guilds/Index.js
@@ -2,43 +2,41 @@ const Discord = require("discord.js"); // require the discord.js wrapper
 const client = new Discord.Client(); // Declare client to be a new Discord Client (bot)
 
 const newUsers = []; // a blank array for storing the newUser data
-/*
-Code Below provides info about the bot 
-once it's ready
-*/
+
+// Code Below provides info about the bot once it's ready
 client.on("ready", () => {
-    console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ\n[INFO] Bot serving on Ready to serve in ${client.guilds.cache.size} servers\n[INFO] Bot serving ${client.users.cache.size} users\n[Invite Link] https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
+  console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ\n[INFO] Bot serving on Ready to serve in ${client.guilds.cache.size} servers\n[INFO] Bot serving ${client.users.cache.size} users\n[Invite Link] https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
 });
-/* 
-Client when detects a message 
-then execute the code */
-client.on("message", (message) => {
+
+//Client when detects a message then execute the code
+client.on("messageCreate", (message) => {
   if (message.content.startsWith("!ping")) {
-  message.channel.send(`The client websocket latency is ${client.ws.ping}ms (values in milliseconds)`);
+    message.channel.send({
+      content: `My ping is \`${client.ws.ping}ms\`!`
+    });
   }
 });
-/* 
-Client when detects 
-a new member join */
+
+// Client when detects a new member join */
 client.on("guildMemberAdd", (member) => {
   const guild = member.guild;
   if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
   newUsers[guild.id].set(member.id, member.user);
-/* 
-Below states if the members who joined are more than 5 in value then
-execute the following code to welcome all 5 together (can be set to any integral value (1,2,3,etc.)
-*/
+
+  // Below states if the members who joined are more than 5 in value then execute the following code to welcome all 5 together (can be set to any integral value (1,2,3,etc.)
   if (newUsers[guild.id].size > 5) {
     const userlist = newUsers[guild.id].map(userlist => userlist.toString()).join(" ");
-    guild.channels.find(channel => channel.name === "welcome").send({embed: {
-  color: 3447003,
-       author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-  title: "This is an embed",
-  description: userlist
-}});
+    guild.channels.find(channel => channel.name === "welcome").send({
+      embeds: {
+        color: 3447003,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        title: "This is an embed",
+        description: userlist
+      }
+    });
     newUsers[guild.id].clear();
   }
 });
@@ -51,4 +49,4 @@ client.on("guildMemberRemove", (member) => {
   if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
-client.login("BOTTOKENHERE"); // Login with the token make sure to add it when starting
+client.login("bot-token-here"); // Login with the token make sure to add it when starting
