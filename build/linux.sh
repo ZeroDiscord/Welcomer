05961127c794f12986da6c024f54b202178cd1c5
ph3b3 <phoebeleong8@gmail.com>
2021-11-24 12:49:01 +1100

    Create linux.sh

---
+++ build/linux.sh
@@ -0,0 +1,6 @@
+apt install nodejs npm -y
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+chmod 777 nodesource_setup.sh
+./nodesource_setup.sh
+apt install nodejs -y
+npm install
